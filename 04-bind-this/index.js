b43eb233d4f4c280a01619561ad6395e7538c2f3
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-06-07 19:03:34 +0200

    07.06

---
+++ 04-bind-this/index.js
@@ -0,0 +1,17 @@
+// ========================= bind() & this ========================= //
+
+// class App {
+// 	constructor() {
+// 		this.serverName = 'localhost';
+// 		document.querySelector('button').addEventListener('click', this.sreverName.bind(this));
+// 	}
+
+// 	getSreverName() {
+// 		console.log(this.serverName);
+// 	}
+// }
+
+// const app = new App();
+// app.getSreverName();
+
+getModulo();
